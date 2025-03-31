b873b30fd6da92391e931fd7385cd05a28502578
shonpuri <shonpuri@gmail.com>
2025-03-31 17:16:37 +0530

    update for no negative stock

---
+++ stock_no_negative/__manifest__.py
@@ -0,0 +1,17 @@
+# ?? 2025-2026 Momentum91 (https://www.momentum91.com)
+# @@Momentum91
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+{
+    "name": "Stock Disallow Negative",
+    "version": "18.0.1.0.2",
+    "category": "Inventory, Logistic, Storage",
+    "license": "AGPL-3",
+    "summary": "Disallow negative stock levels by default",
+    "author": "Momentum91",
+    "website": "https://www.momentum91.com/",
+    "depends": ["stock"],
+    "data": ["views/product_product_views.xml", "views/stock_location_views.xml"],
+    "installable": True,
+}
